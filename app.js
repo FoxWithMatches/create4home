0c132375b1f9ee9237a6457a912887515deef29e
FoxWithMatches <freiheiit92@mail.ru>
2024-07-03 13:10:46 +0300

    animation blocks

--- app.js
+++ app.js
@@ -61,4 +61,24 @@ function checkScrolls(scrollPos, introH) {
   }
 }
 
+//animation
+
+  function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        change.target.classList.add('element_show');
+      } else {
+        change.target.classList.remove('element_show');
+      }
+    });
+  }
+
+  let options = { threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.element_animation');
+
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+  
 })
